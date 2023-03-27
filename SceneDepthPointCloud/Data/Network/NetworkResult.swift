e481cbb28ae4d5cc00236c1641c2a2429144900c
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-03-27 16:08:52 +0900

    #1 feat: Network request 기본함수 생성

---
+++ SceneDepthPointCloud/Data/Network/NetworkResult.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkResult.swift
+//  SceneDepthPointCloud
+//
+//  Created by Kang Minsang on 2023/03/27.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkResult {
+    let data: Data?
+    let status: NetworkStatus
+}

be59755eb408fb475affaa9db113115a23369304
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-03-28 20:48:39 +0900

    #1 feat: StatusIndicatorLabel 생성, Global Thread 및 Main Thread 로직 수정

--- SceneDepthPointCloud/Data/ApiService/MainApiService.swift
+++ SceneDepthPointCloud/Data/ApiService/MainApiService.swift
@@ -11,8 +11,9 @@ import Foundation
 final class MainApiService {
     /// ply 데이터를 서버로 전송
     func uploadPlyData(fileName: String, fileData: Data, completion: @escaping (NetworkResult) -> Void) {
-        Network.uploadData(url: NetworkURL.uploadPly, fileName: fileName, fileData: fileData) { result in
-            completion(result)
-        }
+//        Network.uploadData(url: NetworkURL.uploadPly, fileName: fileName, fileData: fileData) { result in
+//            completion(result)
+//        }
+        completion(NetworkResult(data: nil, status: .ERROR))
     }
 }

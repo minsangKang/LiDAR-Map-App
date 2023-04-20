33f6ca40abfb70c444d531c58b1ecd35b0c9dc14
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-04-21 00:25:41 +0900

    #9 feat: titleLabel 표시 구현

--- SceneDepthPointCloud/Presentation/SelectLocation/SelectLocationVC.swift
+++ SceneDepthPointCloud/Presentation/SelectLocation/SelectLocationVC.swift
@@ -12,14 +12,34 @@ import Combine
 /// 측정위치 선택화면의 UI 및 UX 담당
 final class SelectLocationVC: UIViewController {
     static let identifier = "SelectLocationVC"
+    /// 화면 상단 타이틀 텍스트
+    private let titleLabel = SelectLocationTitleLabel()
     /// 측정위치 선택화면 관련된 로직담당 객체
     private var viewModel: SelectLocationVM!
     
+    /// SelectLocationVC 최초 접근시 configure
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureUI()
     }
-    
+}
+
+// MARK: INPUT
+extension SelectLocationVC {
     func configureViewModel(_ viewModel: SelectLocationVM) {
         self.viewModel = viewModel
+    }
+}
+
+// MARK: Configure
+extension SelectLocationVC {
+    /// SelectLocationVC 표시할 UI 설정
+    private func configureUI() {
+        // titleLabel
+        self.view.addSubview(self.titleLabel)
+        NSLayoutConstraint.activate([
+            self.titleLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            self.titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        ])
     }
 }

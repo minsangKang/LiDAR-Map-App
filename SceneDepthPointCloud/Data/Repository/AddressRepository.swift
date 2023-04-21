0b57b6793c33b013c218336bcd2a433aa14cd7d6
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-04-21 12:21:21 +0900

    #9 feat: AddressRepository 함수명, Interface 생성

---
+++ SceneDepthPointCloud/Data/Repository/AddressRepository.swift
@@ -0,0 +1,16 @@
+//
+//  AddressRepository.swift
+//  SceneDepthPointCloud
+//
+//  Created by Kang Minsang on 2023/04/21.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import Foundation
+
+/// 주소 정보 Repository
+final class AddressRepository {
+    func fetchAddress(from location: LocationData) -> String? {
+        return nil
+    }
+}
---
+++ SceneDepthPointCloud/Presentation/SelectLocation/Protocol/AddressRepositoryInterface.swift
@@ -0,0 +1,15 @@
+//
+//  AddressRepositoryInterface.swift
+//  SceneDepthPointCloud
+//
+//  Created by Kang Minsang on 2023/04/21.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import Foundation
+
+/// Data Layer 을 간접접근하기 위한 인터페이스
+protocol AddressRepositoryInterface: AnyObject {
+    /// LocationData로부터 도로명주소를 수신하는 함수
+    func fetchAddress(from location: LocationData) -> String?
+}

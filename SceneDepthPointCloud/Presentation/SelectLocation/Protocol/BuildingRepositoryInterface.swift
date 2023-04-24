2b96656a18918b3747f0aec42ebe24be4e5f025e
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-04-24 13:57:03 +0900

    #11 feat: BuildingRepositoryInterface 생성

---
+++ SceneDepthPointCloud/Presentation/SelectLocation/Protocol/BuildingRepositoryInterface.swift
@@ -0,0 +1,15 @@
+//
+//  BuildingRepositoryInterface.swift
+//  SceneDepthPointCloud
+//
+//  Created by Kang Minsang on 2023/04/24.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import Foundation
+
+/// Data Layer를 간접접근하기 위한 인터페이스
+protocol BuildingRepositoryInterface: AnyObject {
+    /// LocationData로부터 100m근방 장소들을 받는 함수
+    func fetchBuildingInfo(from location: LocationData, completion: @escaping (Result<BuildingInfo, FetchError>) -> Void)
+}

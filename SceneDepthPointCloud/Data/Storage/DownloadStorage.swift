4a5805c52495deba50487601382e5a026ea6c7a8
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-06-11 23:31:51 +0900

    #36 feat: DownloadStorage: getURL, remove 함수 생성

--- SceneDepthPointCloud/Data/Storage/DownloadStorage.swift
+++ SceneDepthPointCloud/Data/Storage/DownloadStorage.swift
@@ -13,48 +13,23 @@ final class DownloadStorage {
     
     static let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("download", isDirectory: false)
     
-    static func save(_ obj: LiDARData) -> Bool {
-        print("---> save to here: \(url)")
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = .prettyPrinted
-        encoder.dateEncodingStrategy = .iso8601
-        
-        do {
-            let data = try encoder.encode(obj)
-            //동일이름의 파일이 있는 경우 삭제
-            if FileManager.default.fileExists(atPath: url.path) {
-                try FileManager.default.removeItem(at: url)
-            }
-            //파일을 저장
-            FileManager.default.createFile(atPath: url.path, contents: data, attributes: nil)
-            return true
-        } catch let error {
-            print("---> Failed to store msg: \(error.localizedDescription)")
-            return false
-        }
-    }
-    
-    static func get() -> LiDARData? {
-        guard FileManager.default.fileExists(atPath: url.path) else { return nil }
-        guard let data = FileManager.default.contents(atPath: url.path) else { return nil }
-        
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .iso8601
-        
-        do {
-            let lidarData = try decoder.decode(LiDARData.self, from: data)
-            return lidarData
-        } catch let error {
-            print("---> Failed to decode msg: \(error.localizedDescription)")
+    static func getURL(fileName: String) -> URL? {
+        let url = url.appendingPathComponent(fileName)
+        if FileManager.default.fileExists(atPath: url.path) {
+            return url
+        } else {
             return nil
         }
     }
     
-    static func remove() {
+    static func remove(fileName: String) -> Bool {
         do {
+            let url = url.appendingPathComponent(fileName)
             try FileManager.default.removeItem(at: url)
+            return true
         } catch let error {
             print("---> Failed to remove msg: \(error.localizedDescription)")
+            return false
         }
     }
 }
